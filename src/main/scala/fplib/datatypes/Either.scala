2058e16862d6db7de994c720e3781e4518b46a8f
kirill5k <immotional@aol.com>
2019-12-08 18:11:06 +0000

    either methods

--- src/main/scala/fplib/datatypes/Either.scala
+++ src/main/scala/fplib/datatypes/Either.scala
@@ -1,5 +1,21 @@
 package fplib.datatypes
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(x) => Right(f(x))
+    case Left(e) => Left(e)
+  }
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(x) => f(x)
+    case Left(e) => Left(e)
+  }
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(x) => Right(x)
+    case _ => b
+  }
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for { x <- this; y <- b } yield f(x,y)
+}
+
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
