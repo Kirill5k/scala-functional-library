c872227f205a9103ebba597e7c09f4bb9f001aaf
kirill5k <immotional@aol.com>
2019-12-08 18:17:33 +0000

    more either methods

--- src/main/scala/fplib/datatypes/Either.scala
+++ src/main/scala/fplib/datatypes/Either.scala
@@ -1,5 +1,7 @@
 package fplib.datatypes
 
+import scala.collection.immutable.List
+
 sealed trait Either[+E, +A] {
   def map[B](f: A => B): Either[E, B] = this match {
     case Right(x) => Right(f(x))
@@ -16,6 +18,12 @@ sealed trait Either[+E, +A] {
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for { x <- this; y <- b } yield f(x,y)
 }
 
-
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    es.foldLeft[Either[E, List[A]]](Right(Nil))((acc, el) => acc.map2(el)(_ :+ _))
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldLeft[Either[E, List[B]]](Right(Nil))((acc, el) => acc.map2(f(el))(_ :+ _))
+}
