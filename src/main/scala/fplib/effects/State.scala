2ec24ea375849a7a7f9b88a91df6564a98352bef
kirill5k <immotional@aol.com>
2019-12-12 17:56:00 +0000

    remaining state functions

--- src/main/scala/fplib/effects/State.scala
+++ src/main/scala/fplib/effects/State.scala
@@ -10,15 +10,14 @@ final case class State[S, +A](run: S => (A, S)) {
     val (a, s1) = run(s)
     f(a).run(s1)
   })
-
-  def zip[B](sb: State[S, B]): State[S, (A, B)] = flatMap(x => sb.map(y => (x,y)))
-
   def zipWith[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(x => sb.map(y => f(x,y)))
+  def zip[B](sb: State[S, B]): State[S, (A, B)] = zipWith(sb)((_,_))
 }
 
 object State {
-  def unit[S, A](a: A): State[S, A] =
-    State(s => (a, s))
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+  def get[S]: State[S, S] = State(s => (s, s))
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
   def map2[S, A, B, C](sa: State[S, A], sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sa.flatMap(x => sb.map(y => f(x,y)))
   def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
