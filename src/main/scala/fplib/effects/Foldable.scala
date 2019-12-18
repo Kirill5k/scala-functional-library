e6c6773fe7ae2c6cd2ce0a7002098dae1d11dbf4
kirill <immotional@aol.com>
2019-12-18 15:05:13 +0000

    foldable

---
+++ src/main/scala/fplib/effects/Foldable.scala
@@ -0,0 +1,18 @@
+package fplib.effects
+
+import scala.collection.immutable.List
+
+trait Foldable[F[_]] {
+  def foldRight[A,B](as: F[A])(z: B)(f: (A,B) => B): B
+  def foldLeft[A,B](as: F[A])(z: B)(f: (B,A) => B): B
+  def foldMap[A,B](as: F[A])(f: A => B)(implicit mb: Monoid[B]): B
+  def concatenate[A](as: F[A])(implicit m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
+}
+
+object Foldable {
+  val foldableList: Foldable[List] = new Foldable[List] {
+    override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = as.foldRight[B](z)(f)
+    override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+    override def foldMap[A, B](as: List[A])(f: A => B)(implicit mb: Monoid[B]): B = foldLeft(as.map(f))(mb.zero)(mb.op)
+  }
+}
