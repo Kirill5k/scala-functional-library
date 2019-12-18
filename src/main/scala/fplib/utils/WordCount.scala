7847bf5e4a2c1598ac845cc01c1864a563afc121
kirill <immotional@aol.com>
2019-12-18 13:05:16 +0000

    word count impl

--- src/main/scala/fplib/utils/WordCount.scala
+++ src/main/scala/fplib/utils/WordCount.scala
@@ -7,10 +7,13 @@ case class Stub(chars: String) extends WordCount
 case class Part(lStub: String, words: Int, rStub: String) extends WordCount
 
 object WordCount {
+  def count(s: String): Int = {
+    val charToStub: Char => WordCount = c => if (c.isWhitespace) Part("", 0, "") else Stub(c.toString)
+    val unstub: String => Int = x => x.length.min(1)
 
-  // convert to indexed sex
-  // map each char to stub
-  // apply monoid
-
-  def count(s: String): Int = ???
+    Monoid.foldMap(s.toIndexedSeq)(charToStub)(Monoid.wordCountMonoid) match {
+      case Stub(x) => unstub(x)
+      case Part(l, c, r) =>unstub(l) + c + unstub(r)
+    }
+  }
 }
