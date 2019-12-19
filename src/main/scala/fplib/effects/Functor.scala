bae9d94dfd1fcab0c25a16d345c192edc63b94a1
kirill <immotional@aol.com>
2019-12-19 08:47:40 +0000

    functor

---
+++ src/main/scala/fplib/effects/Functor.scala
@@ -0,0 +1,12 @@
+package fplib.effects
+
+trait Functor[F[_]] {
+  def map[A,B](fa: F[A])(f: A => B): F[B]
+  def unzip[A,B](fab: F[(A, B)]): (F[A], F[B]) = (map(fab)(_._1), map(fab)(_._2))
+}
+
+object Functor {
+  implicit val listFunctor = new Functor[List] {
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+}
