f9c0baf4778b171228d97756dd9c2af30e648cac
kirill <immotional@aol.com>
2019-12-22 11:21:23 +0000

    IO monad

--- src/main/scala/fplib/effects/IO.scala
+++ src/main/scala/fplib/effects/IO.scala
@@ -1,12 +1,15 @@
 package fplib.effects
 
-trait IO { self =>
-  def run: Unit
-  def ++(io: IO): IO = new IO {
-    override def run: Unit = { self.run; io.run }
-  }
+import fplib.abstractions.Monad
+
+trait IO[A] { self =>
+  def run: A
+  def map[B](f: A => B): IO[B] = new IO[B] { def run: B = f(self.run) }
+  def flatMap[B](f: A => IO[B]): IO[B] = new IO[B] { def run: B = f(self.run).run }
 }
 
-object IO {
-  def empty: IO = new IO { def run: Unit = () }
+object IO extends Monad[IO] {
+  override def unit[A](a: => A): IO[A] = new IO[A] { def run = a }
+  override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa.flatMap(f)
+  def apply[A](a: => A): IO[A] = unit(a)
 }
